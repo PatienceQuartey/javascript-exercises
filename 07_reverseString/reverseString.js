eb6c62a2e7778f7590653ef526ebf8f1c4dde1ce
Patience Quartey <patiencenkquartz@gmail.com>
2025-11-08 17:07:09 +0000

    Added an empty split() method to the function to output an array of letters.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(theWord) {
+    return theWord.split('')
+
+
 
 };
+
+
+console.log(
+    reverseString("hello")
+)
 
 // Do not edit below this line
 module.exports = reverseString;

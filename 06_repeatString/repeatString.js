4c21280682154adb7686b7ba4af6e4a988995309
Patience Quartey <patiencenkquartz@gmail.com>
2025-11-08 16:32:01 +0000

    Amended IF STATEMENT to enable user to iterate 0 times.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,8 +1,8 @@
 const repeatString = function(string , num) {
     let userWord = ""
-    if(num <= 0){
-        return "Please input number 1 and over!"
-    }else if(num > 0){
+    if(num < 0){
+        return "Please input number 0 and over!"
+    }else if(num >= 0){
         for (let i = 0; i < num; i++){
             userWord += string
 
@@ -15,7 +15,7 @@ const repeatString = function(string , num) {
 
 
 
-console.log(repeatString("hey" , 3))
+console.log(repeatString("bye" , 0))
 
 // Do not edit below this line
 module.exports = repeatString;

a3cabc3b73b231a8c2a352f4cbe7f43a3a2be139
Patience Quartey <patiencenkquartz@gmail.com>
2025-11-08 16:23:44 +0000

    Created a function using IF statement and a FOR LOOP to catch errors if user inputs 0 or less, and iterate x amount of times if the user inputs a number 1 and over.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string , num) {
+    let userWord = ""
+    if(num <= 0){
+        return "Please input number 1 and over!"
+    }else if(num > 0){
+        for (let i = 0; i < num; i++){
+            userWord += string
+
+            
+        }
+    }
+    return userWord
 
 };
+
+
+
+console.log(repeatString("hey" , 3))
 
 // Do not edit below this line
 module.exports = repeatString;

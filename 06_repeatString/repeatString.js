f1491ef3d1381f34bdd6317a1b49b77d93caaf3f
Patience Quartey <patiencenkquartz@gmail.com>
2025-11-08 16:35:52 +0000

    Amended the error message.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,7 +1,7 @@
 const repeatString = function(string , num) {
     let userWord = ""
     if(num < 0){
-        return "Please input number 0 and over!"
+        return "ERROR"
     }else if(num >= 0){
         for (let i = 0; i < num; i++){
             userWord += string
@@ -15,7 +15,7 @@ const repeatString = function(string , num) {
 
 
 
-console.log(repeatString("bye" , 0))
+console.log(repeatString("goodbye" , -1))
 
 // Do not edit below this line
 module.exports = repeatString;
